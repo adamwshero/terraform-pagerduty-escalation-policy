e186ce7123645a20227c1062b727909df76eeb6e
adamwshero <cloudarmy.io@gmail.com>
2022-09-26 14:08:56 -0600

    .

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,21 @@
-output "pagerduty_escalation_policy_ids" {
+# output "pagerduty_escalation_policy_ids" {
+#   description = "Ids of PagerDuty Escalation Policies."
+#   value = tomap({
+#     for k, policies in pagerduty_escalation_policy.this : k => {
+#       name                        = policies.name
+#       num_loops                   = policies.num_loops
+#       teams                       = policies.teams
+#       escalation_delay_in_minutes = policies.escalation_delay_in_minutes
+#       type                        = policies.type
+#       id                          = policies.id
+#     }
+#   })
+# }
+
+output "pagerduty_escalation_policies" {
   description = "Ids of PagerDuty Escalation Policies."
-  value = tomap({
-    for k, policies in pagerduty_escalation_policy.this : k => {
-      name                        = policies.name
-      description                 = policies.description
-      num_loops                   = policies.num_loops
-      teams                       = policies.teams
-      escalation_delay_in_minutes = policies.escalation_delay_in_minutes
-      type                        = policies.type
-      id                          = policies.id
-    }
-  })
+    value = {
+        for pagerduty_escalation_policies in keys(var.escalation_policies):
+          pagerduty_escalation_policies => pagerduty_escalation_policy.this[pagerduty_escalation_policies].*[0].*
+  }
 }
